c3bcbfa4be08e978a909541b5356c48e9faab631
Richard Vellojin <richrdvellojin@gmail.com>
2024-09-29 17:49:53 -0500

    Agregando nuevas rutas

--- SCS-Hotels/src/routes/auth.routes.js
+++ SCS-Hotels/src/routes/auth.routes.js
@@ -3,10 +3,18 @@ import { Router, } from "express";
 const router = Router();
 
 router.get('/login', (req,res) =>{
-    res.send('Esto es la ruta del login');
+    res.send('Esto es la ruta del login para entrenar');
+});
+
+router.post('/login', (req,res) =>{
+    res.send('Esto es la ruta del login para entrenar');
 });
 
 router.get('/signup', (req,res) =>{
+    res.send('Esto es la ruta de registro');
+});
+
+router.post('/signup', (req,res) =>{
     res.send('Esto es la ruta de registro');
 });
 
